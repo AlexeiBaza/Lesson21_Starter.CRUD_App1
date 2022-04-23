77659b590e88d277ab52b85287d3b35212e663ab
Aleksey Simukov <AlexeiBazaGM@gmail.com>
2022-04-23 17:55:10 +0300

    Initial commit

---
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    //получим всех людей из DAO и передадим на отображение в представление
+    @GetMapping()
+    public String index(Model model) {
+        //под ключем people будет лежать список из людей
+        model.addAttribute("people", personDAO.index());//получим людей и положим их в модель чтобы передать их в представление
+        return "people/index";//вернем название того шаблона который будет отображать список из людей
+    }
+
+    //получим одного человека по его id из DAO и передадим на отображение в представление
+    @GetMapping("/{id}")//во время запска приложения id поместиться в аргументы метода
+    public String show(@PathVariable("id") int id, Model model) {//с помощью аннотации PathVariable извлечем id из URL и получим к нему доступ внутри метода
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+}
